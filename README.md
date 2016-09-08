547868554a0f03240825374e5017db691c3b6092
sahiljain112 <sahiljain112@gmail.com>
2016-09-08 22:31:06 +0530

    Basic word count addition

---
+++ README.md
@@ -0,0 +1,2 @@
+# atom word count
+A word count toggle bar for atom. Based on tutorial in the atom flight manual
---
+++ spec/sahil-word-count-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import SahilWordCountView from '../lib/sahil-word-count-view';
+
+describe('SahilWordCountView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('life');
+  });
+});
